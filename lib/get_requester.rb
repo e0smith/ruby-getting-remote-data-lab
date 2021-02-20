e44b114ab695907d853fe3be2e3c3f71b23a953d
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-02-20 13:21:14 -0500

    commit tree 3d18039d6abf199e0a338650a88a85bdda47b47c

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,31 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+require 'pry'
+class GetRequester
+  attr_accessor :url
+
+  def initialize(url)
+    @url = url
+  end
+    # URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+  def parse_json
+    new_array = []
+    request = JSON.parse(get_response_body)
+    request.collect do |req|
+      new_array.push(({"name" => req["name"], "occupation" => req["occupation"]}))
+    end
+    # binding.pry
+    return new_array
+  end
+
+
+end
